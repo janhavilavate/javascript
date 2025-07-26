59d84cdb0f438f555f74f99ec9c308436c96f47b
janhavilavate <lavatejanhavi@gmail.com>
2025-07-26 11:24:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+//1.answer
+let a=10;
+let b=20;
+console.log("additin is:", a+b);
+
+//2.answer 
+let c=17;
+let d=3;
+console.log("reminder is:", c%d);
+
+//3.answer
+let e=2;
+let f=3;
+let g=4;
+console.log("multiplication of 2*3*4 is:", e*f*g);
+
+//4.answer
+let h=250;
+let i=100;
+console.log("Subtraction of 250-100 is:", h-i);
+
+//5.answer
+let j=5;
+let k=++j;
+console.log(j);
+answer :6
+
+//6.answer
+let l=20;
+l+=10;
+console.log("The value of l is:", l)
+
+//7.answer
+let m=7;
+m*=2;
+console.log("The double value of 7 is:", m);
+
+//8.answer
+/*let n = 10;
+o -= 3;
+console.log(n)
+answer :7 (n=-3)*/
+
+//9.answer
+let p=5;
+let q='5';
+console.log(p==q);
+console.log(p===q);
+
+//10.answer
+let r=25;
+let s=14;
+console.log(r>s);
+
+//11.answer
+let t=7;
+let u=12;
+console.log(t<u);
+
+//12.answer
+console.log(5 === "5")
+answer :false
+
+//13.answer 
+let num=20;
+console.log(num>10 && num<50);
+
+//14.answer 
+let num2=15;
+console.log(num>10 || num<100); 
+
+//16.answer
+let value = true;
+console.log(!value);
+
+//17.answer
+let x =6;
+console.log("Even or Odd:", x % 2 === 0 ? "Even": "Odd");
+
+//18.answer
+let age=18;
+result=age>=18;
+console.log("result:",age>=18 ?"Yes":"No");
+
+//19.answer
+let y=5;
+let z=10;
+console.log("Greater number:",y > z ? y : z );
+
+//20.answer
+let A=5;
+let B=A++ + ++A;
+console.log(B);
