618a23a4f263cc1bb73b79806f9209df9a01058b
janhavilavate <lavatejanhavi@gmail.com>
2025-08-05 22:04:18 +0530

    Add files via upload

---
+++ Stringmethod.js
@@ -0,0 +1,24 @@
+//1.answer
+let a="janhavi";
+console.log(a.toUpperCase());
+//2.answer
+let b="LAVATE";
+console.log(b.toLowerCase());
+//3.answer
+let str=prompt("Enter name")
+console.log(str.length);
+//5.answer
+let d="Pravin"
+console.log(d.endsWith("n"));
+//6.answer
+let place="Latur"
+console.log(place.startsWith("l"));
+//9.answer
+let c=prompt("Enter a name")
+for(let i=0; i<c.length; i++){
+    console.log(c[i]);
+}
+//10.answer
+let x = prompt("Enter string name");
+let z = x.split(" ");
+console.log("List of words:", z);
