c55b2335148a42fcdfb2fc6419d458874baae014
janhavilavate <lavatejanhavi@gmail.com>
2025-08-06 10:51:02 +0530

    Update Stringmethod.js

--- Stringmethod.js
+++ Stringmethod.js
@@ -7,12 +7,26 @@ console.log(b.toLowerCase());
 //3.answer
 let str=prompt("Enter name")
 console.log(str.length);
+//4.answer
+count=0;
+let str4 = prompt("Enter your name");
+for ("let var of str:"){
+    if str4.includes(the) {
+        count=count+1
+    }
+}
 //5.answer
 let d="Pravin"
 console.log(d.endsWith("n"));
 //6.answer
 let place="Latur"
 console.log(place.startsWith("l"));
+//7.answer
+let e = prompt("Enter a string remove the spaces before/after:");
+console.log("Trimmed string:", e.trim());
+//8.answer
+let k = "replace all spaces with hyphens";
+console.log("Hyphenated string:", k.replace(a, "-"));
 //9.answer
 let c=prompt("Enter a name")
 for(let i=0; i<c.length; i++){
